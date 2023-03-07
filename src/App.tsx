639bbad377b1405387af5105703a2cd3a8e6d852
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-03-07 20:50:44 +0000

    clerk boilerplate setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react";
+
+
+const clerkPubKey = import.meta.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 function App() {
   return (
-    <div className="App">
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+
+    <ClerkProvider publishableKey={"pk_test_Zmxvd2luZy1ob3JuZXQtOC5jbGVyay5hY2NvdW50cy5kZXYk"}>
+      <SignedIn>
+        <Hello />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+
+    </ClerkProvider>
+
+
   )
+}
+
+function Hello() {
+
+  const { user } = useUser();
+  
+  return (
+    <div className="App">
+      <UserButton />
+      {user ? <h1 className="text-3xl font-bold underline">Hello, {user.firstName}!</h1> : null}
+    </div>);
 }
 
 export default App
